89cc84617d896a8d717e06101585d68155d6bb7d
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-11 20:04:24 -0300

    Feature/vehicle recylcerview (#3)
    * add vehicle list
    
    * change to databinding
    
    * add vehicle adapter

--- app/src/main/java/com/example/appto/MainActivity.kt
+++ app/src/main/java/com/example/appto/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.example.appto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
