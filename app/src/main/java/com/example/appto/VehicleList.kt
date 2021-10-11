4fd3c4bdb37cee385e9cc3f77aafe2ca0ecb32f2
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-11 12:52:58 -0300

    add vehicle list

---
+++ app/src/main/java/com/example/appto/VehicleList.kt
@@ -0,0 +1,11 @@
+package com.example.appto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VehicleList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_list)
+    }
+}
