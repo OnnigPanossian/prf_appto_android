3bcba6ffc1c1eda7b58334d05ca850e21d76d5d0
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-11 12:54:01 -0300

    change to databinding

--- app/src/main/java/com/example/appto/Login.kt
+++ app/src/main/java/com/example/appto/Login.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appto.databinding.ActivityLoginBinding
 
+
 class Login : AppCompatActivity() {
+
     private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
--- app/src/main/java/com/example/appto/VehicleList.kt
+++ app/src/main/java/com/example/appto/VehicleList.kt
@@ -2,10 +2,15 @@ package com.example.appto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appto.databinding.ActivityVehicleListBinding
 
 class VehicleList : AppCompatActivity() {
+
+    lateinit var binding: ActivityVehicleListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vehicle_list)
+        binding = ActivityVehicleListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
