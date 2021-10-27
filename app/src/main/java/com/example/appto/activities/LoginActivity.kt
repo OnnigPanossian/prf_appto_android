bba73a9e7ed75e90225edffd2db64c42ac434977
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-10-27 20:31:41 -0300

    add login redirect to map

--- app/src/main/java/com/example/appto/activities/LoginActivity.kt
+++ app/src/main/java/com/example/appto/activities/LoginActivity.kt
@@ -2,12 +2,18 @@ package com.example.appto.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.appto.databinding.ActivityLoginBinding
+import com.example.appto.viewmodels.UserViewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,45 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         setRegisterRedirectEvent()
+
+        userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+
+        binding.buttonLog.setOnClickListener {
+            val userEmail = binding.inputMailLog.text.toString()
+            val userPass = binding.inputPassLog.text.toString()
+
+            val validInputs = validateInputs(userEmail, userPass)
+
+            if (validInputs) {
+                // val ok: Boolean = userViewModel.register(userEmail, userPass)
+
+                if (true) {
+                    startActivity(Intent(this, MapsActivity::class.java))
+                } else {
+                    Log.i("Matias: ", "4")
+                    Toast.makeText(this, "Datos de usuario inválidos", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun validateInputs(email: String, pass: String): Boolean {
+        var isEmailValid = true
+        var isPasswordValid = true
+        binding.inputPassLog.error = null
+        binding.inputMailLog.error = null
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            isEmailValid = false;
+            binding.inputMailLog.error = "Formato de email inválido"
+        }
+
+        if (pass.isEmpty()) {
+            isPasswordValid = false
+            binding.inputPassLog.error = "Debes ingresar la contraseña"
+        }
+
+        return isEmailValid && isPasswordValid
     }
 
     private fun setRegisterRedirectEvent() {
@@ -24,4 +69,5 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
+
 }
