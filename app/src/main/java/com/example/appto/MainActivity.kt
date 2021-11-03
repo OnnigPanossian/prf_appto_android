ec7c6df0082c49ddbe191b48a1b82dca1220e3fa
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-11-02 23:56:07 -0300

    add navigation header and menu content

--- app/src/main/java/com/example/appto/MainActivity.kt
+++ app/src/main/java/com/example/appto/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.appto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
+import androidx.core.view.GravityCompat
 import com.example.appto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val drawerLayout = binding.drawerLayout
+        binding.imageMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Si ponemos iconos .png de colores
+        // val navigationView = binding.navigationView
+        // navigationView.itemIconTintList = null
     }
+
+
 
 }
