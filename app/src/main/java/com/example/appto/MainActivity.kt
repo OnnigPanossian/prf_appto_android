91682480b74bbc637ed0ced7973e28799118660b
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-11-03 00:14:09 -0300

    Feature/navigation drawer (#15)
    * add dependencies and navigation drawer xml
    
    * add navigation header and menu content
    
    * add navication controller

--- app/src/main/java/com/example/appto/MainActivity.kt
+++ app/src/main/java/com/example/appto/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.appto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.appto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.hide()
+
+        val drawerLayout = binding.drawerLayout
+        binding.imageMenu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        val navigationView = binding.navigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id)
+        NavigationUI.setupWithNavController(navigationView, navHostFragment!!.findNavController())
+
+        // Si ponemos iconos .png de colores
+        // navigationView.itemIconTintList = null
+
     }
+
+
 
 }
