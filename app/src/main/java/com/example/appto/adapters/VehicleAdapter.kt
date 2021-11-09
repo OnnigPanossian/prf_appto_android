620e7ccd66dfc6f7d51ed2881a961cd04d9ac99f
matibjolivera <matibjolivera@gmail.com>
2021-11-08 22:28:30 -0300

    feature/book: reserva de vehículos

--- app/src/main/java/com/example/appto/adapters/VehicleAdapter.kt
+++ app/src/main/java/com/example/appto/adapters/VehicleAdapter.kt
@@ -1,10 +1,17 @@
 package com.example.appto.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appto.databinding.VehicleListItemBinding
 import com.example.appto.models.Vehicle
+import com.example.appto.viewmodels.VehicleViewModel
+import com.example.appto.services.vehicleService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class VehicleAdapter(private val vList: List<Vehicle>) :
     RecyclerView.Adapter<VehicleAdapter.ViewHolder>() {
@@ -33,6 +40,17 @@ class VehicleAdapter(private val vList: List<Vehicle>) :
             binding.tvCategory.text = vehicle.category
             (vehicle.brand + " " + vehicle.model).also { binding.tvModel.text = it }
             ("Año: " + vehicle.year.toString()).also { binding.tvYear.text = it }
+
+            binding.btnReserve.setOnClickListener {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val res = vehicleService.book(vehicle.id)
+                    if (res.isSuccessful) {
+                        Log.i("OK", "OK")
+                    } else {
+                        Log.e("ERROR", "Error")
+                    }
+                }
+            }
 
             binding.executePendingBindings()
         }
--- app/src/main/java/com/example/appto/services/VehicleService.kt
+++ app/src/main/java/com/example/appto/services/VehicleService.kt
@@ -2,7 +2,9 @@ package com.example.appto.services
 
 import com.example.appto.clients.Retrofit.Companion.restClient
 import com.example.appto.models.Vehicle
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface VehicleService {
@@ -14,6 +16,9 @@ interface VehicleService {
 
     @GET("vehicles/{id}")
     fun getById(@Path("id") vehicle: String): String
+
+    @POST("vehicles/{id}/book")
+    suspend fun book(@Path("id") vehicle: String): Response<Void>
 }
 
 val vehicleService: VehicleService = restClient.create(VehicleService::class.java)
