037eeccf54a119fde163bde875dcf0727b0b08c8
OnnigPanossian <onnig.panossian@mercadolibre.com>
2021-11-15 02:10:30 -0300

    .

--- app/src/main/java/com/example/appto/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/appto/fragments/MapsFragment.kt
@@ -75,7 +75,6 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
             val dialog = FiltersDialogFragment()
             dialog.show(childFragmentManager, "FiltersDialogFragment")
         }
-        setupMap()
 
         return binding.root
     }
@@ -87,6 +86,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
             if (location != null) {
                 userLocation.latitude = location.latitude
                 userLocation.longitude = location.longitude
+                setupMap()
             }
         }
     }
